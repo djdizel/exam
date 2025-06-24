0dee6b62ed222d58dcf6653678e72ccfddf05e86
djdizel <yarmisikov@gmail.com>
2025-06-25 02:06:20 +0300

    +

---
+++ Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    static class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Ошибка: деление на ноль!");
+                return 0;
+            }
+            return a / b;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите первое число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Сложение: " + a + " + " + b + " = " + Calculator.Add(a, b));
+            Console.WriteLine("Вычитание: " + a + " - " + b + " = " + Calculator.Subtract(a, b));
+            Console.WriteLine("Умножение: " + a + " * " + b + " = " + Calculator.Multiply(a, b));
+            Console.WriteLine("Деление: " + a + " / " + b + " = " + Calculator.Divide(a, b));
         }
     }
 }
