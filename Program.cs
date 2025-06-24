06ada7721b6a4327468f54fd5b3ae4e431135d16
djdizel <yarmisikov@gmail.com>
2025-06-25 02:25:29 +0300

    +

--- Program.cs
+++ Program.cs
@@ -10,6 +10,35 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите часы для первого времени: ");
+            int h1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты для первого времени: ");
+            int m1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите секунды для первого времени: ");
+            int s1 = Convert.ToInt32(Console.ReadLine());
+
+            Time time1 = new Time(h1, m1, s1);
+
+            Console.Write("Введите часы для второго времени: ");
+            int h2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты для второго времени: ");
+            int m2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите секунды для второго времени: ");
+            int s2 = Convert.ToInt32(Console.ReadLine());
+
+            Time time2 = new Time(h2, m2, s2);
+
+            Console.WriteLine("\nПервое время: ");
+            time1.ShowTime();
+            Console.WriteLine("В секундах: " + time1.ToSeconds());
+
+            Console.WriteLine("\nВторое время: ");
+            time2.ShowTime();
+            Console.WriteLine("В секундах: " + time2.ToSeconds());
+
+            Console.WriteLine("\nСумма времён: ");
+            Time sum = time1.Add(time2);
+            sum.ShowTime();
         }
     }
 }
---
+++ Time.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Time
+    {
+        private int hours;
+        private int minutes;
+        private int seconds;
+
+        public Time(int hours, int minutes, int seconds)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            Normalize();
+        }
+
+        private void Normalize()
+        {
+            int totalSeconds = ToSeconds();
+            if (totalSeconds < 0)
+            {
+                totalSeconds = 0;
+            }
+            hours = totalSeconds / 3600;
+            totalSeconds %= 3600;
+            minutes = totalSeconds / 60;
+            seconds = totalSeconds % 60;
+        }
+
+        public int ToSeconds()
+        {
+            return hours * 3600 + minutes * 60 + seconds;
+        }
+
+        public Time Add(Time other)
+        {
+            int totalSeconds = ToSeconds() + other.ToSeconds();
+            return new Time(totalSeconds / 3600, (totalSeconds % 3600) / 60, totalSeconds % 60);
+        }
+
+        public void ShowTime()
+        {
+            Console.WriteLine(String.Format("{0:D2}:{1:D2}:{2:D2}", hours, minutes, seconds));
+        }
+    }
+}
