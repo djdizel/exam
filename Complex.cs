867a8140ea797d115e208526318041907ffb7a35
djdizel <yarmisikov@gmail.com>
2025-06-25 02:18:44 +0300

    +

---
+++ Complex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Complex
+    {
+        private double real;
+        private double imaginary;
+
+        public Complex(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public Complex Add(Complex other)
+        {
+            double newReal = real + other.real;
+            double newImaginary = imaginary + other.imaginary;
+            return new Complex(newReal, newImaginary);
+        }
+
+        public Complex Subtract(Complex other)
+        {
+            double newReal = real - other.real;
+            double newImaginary = imaginary - other.imaginary;
+            return new Complex(newReal, newImaginary);
+        }
+
+        public Complex Multiply(Complex other)
+        {
+            double newReal = real * other.real - imaginary * other.imaginary;
+            double newImaginary = real * other.imaginary + imaginary * other.real;
+            return new Complex(newReal, newImaginary);
+        }
+
+        public override string ToString()
+        {
+            if (imaginary >= 0)
+            {
+                return real + " + " + imaginary + "i";
+            }
+            else
+            {
+                return real + " - " + Math.Abs(imaginary) + "i";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите действительную часть первого числа: ");
+            double real1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите мнимую часть первого числа: ");
+            double imag1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите действительную часть второго числа: ");
+            double real2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите мнимую часть второго числа: ");
+            double imag2 = Convert.ToDouble(Console.ReadLine());
+
+            Complex complex1 = new Complex(real1, imag1);
+            Complex complex2 = new Complex(real2, imag2);
+
+            Console.WriteLine("Первое число: " + complex1.ToString());
+            Console.WriteLine("Второе число: " + complex2.ToString());
+            Console.WriteLine("Сложение: " + complex1.Add(complex2).ToString());
+            Console.WriteLine("Вычитание: " + complex1.Subtract(complex2).ToString());
+            Console.WriteLine("Умножение: " + complex1.Multiply(complex2).ToString());
         }
     }
 }
