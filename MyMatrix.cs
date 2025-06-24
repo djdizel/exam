d5a1a733c2101b3fcbae402fb18dc58de8fe68c9
djdizel <yarmisikov@gmail.com>
2025-06-25 02:19:52 +0300

    +

---
+++ MyMatrix.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class MyMatrix
+    {
+        private double[,] data;
+        private int rows;
+        private int cols;
+
+        public MyMatrix(int rows, int cols)
+        {
+            this.rows = rows;
+            this.cols = cols;
+            data = new double[rows, cols];
+        }
+
+        public void SetElement(int row, int col, double value)
+        {
+            if (row >= 0 && row < rows && col >= 0 && col < cols)
+            {
+                data[row, col] = value;
+            }
+        }
+
+        public MyMatrix Add(MyMatrix other)
+        {
+            if (rows != other.rows || cols != other.cols)
+            {
+                Console.WriteLine("Ошибка: матрицы должны иметь одинаковые размеры!");
+                return null;
+            }
+
+            MyMatrix result = new MyMatrix(rows, cols);
+            int i, j;
+            for (i = 0; i < rows; i++)
+            {
+                for (j = 0; j < cols; j++)
+                {
+                    result.data[i, j] = data[i, j] + other.data[i, j];
+                }
+            }
+            return result;
+        }
+
+        public MyMatrix Subtract(MyMatrix other)
+        {
+            if (rows != other.rows || cols != other.cols)
+            {
+                Console.WriteLine("Ошибка: матрицы должны иметь одинаковые размеры!");
+                return null;
+            }
+
+            MyMatrix result = new MyMatrix(rows, cols);
+            int i, j;
+            for (i = 0; i < rows; i++)
+            {
+                for (j = 0; j < cols; j++)
+                {
+                    result.data[i, j] = data[i, j] - other.data[i, j];
+                }
+            }
+            return result;
+        }
+
+        public MyMatrix Multiply(MyMatrix other)
+        {
+            if (cols != other.rows)
+            {
+                Console.WriteLine("Ошибка: количество столбцов первой матрицы должно быть равно количеству строк второй матрицы!");
+                return null;
+            }
+
+            MyMatrix result = new MyMatrix(rows, other.cols);
+            int i, j, k;
+            for (i = 0; i < rows; i++)
+            {
+                for (j = 0; j < other.cols; j++)
+                {
+                    result.data[i, j] = 0;
+                    for (k = 0; k < cols; k++)
+                    {
+                        result.data[i, j] += data[i, k] * other.data[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+
+        public void Print()
+        {
+            int i, j;
+            for (i = 0; i < rows; i++)
+            {
+                for (j = 0; j < cols; j++)
+                {
+                    Console.Write(String.Format("{0:F2}", data[i, j]) + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,62 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество строк первой матрицы: ");
+            int rows1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов первой матрицы: ");
+            int cols1 = Convert.ToInt32(Console.ReadLine());
+
+            MyMatrix matrix1 = new MyMatrix(rows1, cols1);
+            int i, j;
+            for (i = 0; i < rows1; i++)
+            {
+                for (j = 0; j < cols1; j++)
+                {
+                    Console.Write("Введите элемент [" + i + "," + j + "]: ");
+                    matrix1.SetElement(i, j, Convert.ToDouble(Console.ReadLine()));
+                }
+            }
+
+            Console.Write("Введите количество строк второй матрицы: ");
+            int rows2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов второй матрицы: ");
+            int cols2 = Convert.ToInt32(Console.ReadLine());
+
+            MyMatrix matrix2 = new MyMatrix(rows2, cols2);
+            for (i = 0; i < rows2; i++)
+            {
+                for (j = 0; j < cols2; j++)
+                {
+                    Console.Write("Введите элемент [" + i + "," + j + "]: ");
+                    matrix2.SetElement(i, j, Convert.ToDouble(Console.ReadLine()));
+                }
+            }
+
+            Console.WriteLine("\nПервая матрица:");
+            matrix1.Print();
+            Console.WriteLine("\nВторая матрица:");
+            matrix2.Print();
+
+            MyMatrix sum = matrix1.Add(matrix2);
+            if (sum != null)
+            {
+                Console.WriteLine("\nСумма матриц:");
+                sum.Print();
+            }
+
+            MyMatrix diff = matrix1.Subtract(matrix2);
+            if (diff != null)
+            {
+                Console.WriteLine("\nРазность матриц:");
+                diff.Print();
+            }
+
+            MyMatrix product = matrix1.Multiply(matrix2);
+            if (product != null)
+            {
+                Console.WriteLine("\nПроизведение матриц:");
+                product.Print();
+            }
         }
     }
 }
