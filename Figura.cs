6b9beae89f4cbd92c9f996fd2447ea4d43503f16
djdizel <yarmisikov@gmail.com>
2025-06-25 02:05:31 +0300

    +

---
+++ Figura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Figura
+    {
+        public void ShowArea(double a)
+        {
+            double area = a * a;
+            Console.WriteLine("Фигура: Квадрат");
+            Console.WriteLine("Площадь: " + String.Format("{0:F2}", area));
+        }
+
+        public void ShowArea(double a, double b)
+        {
+            double area = a * b;
+            Console.WriteLine("Фигура: Прямоугольник");
+            Console.WriteLine("Площадь: " + String.Format("{0:F2}", area));
+        }
+
+        public void ShowArea(double a, double b, double h)
+        {
+            double area = (a + b) * h / 2;
+            Console.WriteLine("Фигура: Трапеция");
+            Console.WriteLine("Площадь: " + String.Format("{0:F2}", area));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Figura figura = new Figura();
+
+            // Площадь квадрата
+            Console.Write("Введите сторону квадрата: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            figura.ShowArea(a);
+
+            // Площадь прямоугольника
+            Console.Write("\nВведите длину прямоугольника: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ширину прямоугольника: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            figura.ShowArea(a, b);
+
+            // Площадь трапеции
+            Console.Write("\nВведите длину первого основания трапеции: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите длину второго основания трапеции: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите высоту трапеции: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            figura.ShowArea(a, b, h);
         }
     }
 }
