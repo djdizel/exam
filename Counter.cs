60920630378f661fbca8fff4f3b11bec3e9b2087
djdizel <yarmisikov@gmail.com>
2025-06-25 02:17:01 +0300

    +

---
+++ Counter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Counter
+    {
+        private int count;
+
+        public Counter(int initialCount)
+        {
+            count = initialCount;
+        }
+
+        public void Increment()
+        {
+            count = count + 1;
+        }
+
+        public void Decrement()
+        {
+            count = count - 1;
+        }
+
+        public int Count
+        {
+            get { return count; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите начальное значение счётчика: ");
+            int initialCount = Convert.ToInt32(Console.ReadLine());
+
+            Counter counter = new Counter(initialCount);
+
+            Console.WriteLine("Текущее значение счётчика: " + counter.Count);
+            counter.Increment();
+            Console.WriteLine("После увеличения: " + counter.Count);
+            counter.Increment();
+            Console.WriteLine("После ещё одного увеличения: " + counter.Count);
+            counter.Decrement();
+            Console.WriteLine("После уменьшения: " + counter.Count);
         }
     }
 }
