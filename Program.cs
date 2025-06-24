667e7b999001ceac8911fb6b21a92a1c1327348f
djdizel <yarmisikov@gmail.com>
2025-06-24 21:57:08 +0300

    +

--- Program.cs
+++ Program.cs
@@ -10,6 +10,26 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите ключ доступа (pro, exp или пусто для бесплатной версии): ");
+            string key = Console.ReadLine().ToLower();
+
+            DocumentWorker worker;
+            if (key == "pro")
+            {
+                worker = new ProDocumentWorker();
+            }
+            else if (key == "exp")
+            {
+                worker = new ExpertDocumentWorker();
+            }
+            else
+            {
+                worker = new DocumentWorker();
+            }
+
+            worker.OpenDocument();
+            worker.EditDocument();
+            worker.SaveDocument();
         }
     }
 }
