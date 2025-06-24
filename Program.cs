bd2352960b6f229a3960e61da71d3bb733619b45
djdizel <yarmisikov@gmail.com>
2025-06-25 02:22:59 +0300

    +

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество магазинов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Shop[] shops = new Shop[n];
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nВведите данные для магазина " + (i + 1) + ":");
+                shops[i] = new Shop("", "", "");
+                shops[i].Input();
+            }
+
+            Console.WriteLine("\nИнформация о магазинах:");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Магазин " + (i + 1) + ":");
+                shops[i].ShowInfo();
+            }
         }
     }
 }
---
+++ Shop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Shop
+    {
+        private string name;
+        private string address;
+        private string productType;
+
+        public Shop(string name, string address, string productType)
+        {
+            this.name = name;
+            this.address = address;
+            this.productType = productType;
+        }
+
+        public void Input()
+        {
+            Console.Write("Введите название магазина: ");
+            name = Console.ReadLine();
+            Console.Write("Введите адрес магазина: ");
+            address = Console.ReadLine();
+            Console.Write("Введите тип товаров: ");
+            productType = Console.ReadLine();
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Название магазина: " + name);
+            Console.WriteLine("Адрес: " + address);
+            Console.WriteLine("Тип товаров: " + productType);
+            Console.WriteLine();
+        }
+    }
+}
