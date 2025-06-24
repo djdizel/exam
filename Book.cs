2b6597b81f73334894fec960443c15d3c717411d
djdizel <yarmisikov@gmail.com>
2025-06-25 02:08:28 +0300

    +

---
+++ Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    struct Book
+    {
+        private string title;
+        private string author;
+        private int year;
+        private int pages;
+
+        public Book(string title, string author, int year, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.year = year;
+            this.pages = pages;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Название: " + title);
+            Console.WriteLine("Автор: " + author);
+            Console.WriteLine("Год издания: " + year);
+            Console.WriteLine("Количество страниц: " + pages);
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество книг: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Book[] books = new Book[n];
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nВведите данные для книги " + (i + 1) + ":");
+                Console.Write("Название: ");
+                string title = Console.ReadLine();
+                Console.Write("Автор: ");
+                string author = Console.ReadLine();
+                Console.Write("Год издания: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Количество страниц: ");
+                int pages = Convert.ToInt32(Console.ReadLine());
+
+                books[i] = new Book(title, author, year, pages);
+            }
+
+            Console.WriteLine("\nИнформация о книгах:");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("Книга " + (i + 1) + ":");
+                books[i].ShowInfo();
+            }
         }
     }
 }
