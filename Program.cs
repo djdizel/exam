7bdeeedcc579706df66b93b966f408712e984af2
djdizel <yarmisikov@gmail.com>
2025-06-24 21:05:22 +0300

    +

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите длину первой стороны: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите длину второй стороны: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+            Console.WriteLine($"Периметр: {rectangle.Perimeter}");
+            Console.WriteLine($"Площадь: {rectangle.Area}");
         }
     }
 }
