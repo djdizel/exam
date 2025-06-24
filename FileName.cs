4e35ee80c1a1dd028b0e40452bdfd1cb55a82258
djdizel <yarmisikov@gmail.com>
2025-06-25 02:17:57 +0300

    +

---
+++ FileName.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Fraction
+    {
+        private int numerator;
+        private int denominator;
+
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                Console.WriteLine("Ошибка: знаменатель не может быть равен нулю!");
+                denominator = 1;
+            }
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public Fraction Add(Fraction other)
+        {
+            int newNumerator = numerator * other.denominator + other.numerator * denominator;
+            int newDenominator = denominator * other.denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public Fraction Subtract(Fraction other)
+        {
+            int newNumerator = numerator * other.denominator - other.numerator * denominator;
+            int newDenominator = denominator * other.denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public Fraction Multiply(Fraction other)
+        {
+            int newNumerator = numerator * other.numerator;
+            int newDenominator = denominator * other.denominator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public Fraction Divide(Fraction other)
+        {
+            if (other.numerator == 0)
+            {
+                Console.WriteLine("Ошибка: деление на ноль!");
+                return new Fraction(0, 1);
+            }
+            int newNumerator = numerator * other.denominator;
+            int newDenominator = denominator * other.numerator;
+            return new Fraction(newNumerator, newDenominator);
+        }
+
+        public override string ToString()
+        {
+            return numerator + "/" + denominator;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите числитель первой дроби: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите знаменатель первой дроби: ");
+            int den1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите числитель второй дроби: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите знаменатель второй дроби: ");
+            int den2 = Convert.ToInt32(Console.ReadLine());
+
+            Fraction fraction1 = new Fraction(num1, den1);
+            Fraction fraction2 = new Fraction(num2, den2);
+
+            Console.WriteLine("Первая дробь: " + fraction1.ToString());
+            Console.WriteLine("Вторая дробь: " + fraction2.ToString());
+            Console.WriteLine("Сложение: " + fraction1.Add(fraction2).ToString());
+            Console.WriteLine("Вычитание: " + fraction1.Subtract(fraction2).ToString());
+            Console.WriteLine("Умножение: " + fraction1.Multiply(fraction2).ToString());
+            Console.WriteLine("Деление: " + fraction1.Divide(fraction2).ToString());
         }
     }
 }
