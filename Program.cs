b78b52275a07f98bcd8396c97bce54992ba20cbf
djdizel <yarmisikov@gmail.com>
2025-06-25 02:16:18 +0300

    +

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Numbers numbers = new Numbers(num1, num2);
+
+            Console.WriteLine("Сложение: " + String.Format("{0:F2}", numbers.Add()));
+            Console.WriteLine("Вычитание: " + String.Format("{0:F2}", numbers.Subtract()));
+            Console.WriteLine("Умножение: " + String.Format("{0:F2}", numbers.Multiply()));
+            Console.WriteLine("Деление: " + String.Format("{0:F2}", numbers.Divide()));
+            Console.WriteLine(numbers.Compare());
         }
     }
 }
