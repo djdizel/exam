1a82ebf38bf8a8b878c2f791cc4342c0bc6d0740
djdizel <yarmisikov@gmail.com>
2025-06-25 18:14:25 +0300

    a

---
+++ DateClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class DateClass
+    {
+        private DateTime data;
+
+        public DateClass()
+        {
+            data = DateTime.Now;
+        }
+
+        public DateClass(int year, int month, int day)
+        {
+            data = new DateTime(year, month, day);
+        }
+
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public DateTime PreviousDay()
+        {
+            return data.AddDays(-1);
+        }
+
+        public DateTime NextDay()
+        {
+            return data.AddDays(1);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,32 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Введите год: ");
+                int year = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите месяц: ");
+                int month = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите день: ");
+                int day = Convert.ToInt32(Console.ReadLine());
+
+                DateClass date = new DateClass(year, month, day);
+                Console.WriteLine($"Текущая дата: {date.Data:dd.MM.yyyy}");
+                Console.WriteLine($"Предыдущий день: {date.PreviousDay():dd.MM.yyyy}");
+                Console.WriteLine($"Следующий день: {date.NextDay():dd.MM.yyyy}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введено не число!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Ошибка: недопустимая дата!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
         }
     }
 }
