6d47c96fecc451540511b0a11187ab1f2c9a347e
djdizel <yarmisikov@gmail.com>
2025-06-25 05:50:04 +0300

    a

---
+++ Accauntant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    enum Post
+    {
+        Менеджер = 160,
+        Разработчик = 180,
+        Тестировщик = 170
+    }
+
+    class Accauntant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            return hours > (int)worker;
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -10,25 +10,35 @@ namespace exam
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите числитель первой дроби: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите знаменатель первой дроби: ");
-            int den1 = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.Write("Введите должность (0 - Менеджер, 1 - Разработчик, 2 - Тестировщик): ");
+                int postIndex = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите количество отработанных часов: ");
+                int hours = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Введите числитель второй дроби: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите знаменатель второй дроби: ");
-            int den2 = Convert.ToInt32(Console.ReadLine());
+                if (postIndex < 0 || postIndex > 2)
+                {
+                    throw new ArgumentException("Недопустимая должность!");
+                }
 
-            Fraction fraction1 = new Fraction(num1, den1);
-            Fraction fraction2 = new Fraction(num2, den2);
-
-            Console.WriteLine("Первая дробь: " + fraction1.ToString());
-            Console.WriteLine("Вторая дробь: " + fraction2.ToString());
-            Console.WriteLine("Сложение: " + fraction1.Add(fraction2).ToString());
-            Console.WriteLine("Вычитание: " + fraction1.Subtract(fraction2).ToString());
-            Console.WriteLine("Умножение: " + fraction1.Multiply(fraction2).ToString());
-            Console.WriteLine("Деление: " + fraction1.Divide(fraction2).ToString());
+                Post worker = (Post)postIndex;
+                Accauntant acc = new Accauntant();
+                bool bonus = acc.AskForBonus(worker, hours);
+                Console.WriteLine("Премия " + (bonus ? "положена" : "не положена"));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введено не число!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
         }
     }
 }
