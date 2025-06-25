35efdac9954608a5d28ceaa0d7bb077e639ce4ba
djdizel <yarmisikov@gmail.com>
2025-06-25 18:06:13 +0300

    a

---
+++ Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Point<T>
+    {
+        private T x;
+        private T y;
+
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Точка: ({x}, {y})");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Point<int> intPoint = new Point<int>(3, 4);
+                Console.WriteLine("Точка с int:");
+                intPoint.Show();
+
+                Point<uint> uintPoint = new Point<uint>(5, 6);
+                Console.WriteLine("\nТочка с uint:");
+                uintPoint.Show();
+
+                Point<double> doublePoint = new Point<double>(2.5, 3.7);
+                Console.WriteLine("\nТочка с double:");
+                doublePoint.Show();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
         }
     }
 }
