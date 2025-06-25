6d2b6a82b42e6218c4e2823425952456aba38a2c
djdizel <yarmisikov@gmail.com>
2025-06-25 05:18:08 +0300

    a

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите длину прямоугольника: ");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ширину прямоугольника: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            RectangleArea rectangle = new RectangleArea(length, width);
+
+            Console.WriteLine("Площадь: " + String.Format("{0:F2}", rectangle.CalculateArea()));
+            Console.WriteLine("Периметр: " + String.Format("{0:F2}", rectangle.CalculatePerimeter()));
+            if (rectangle.IsSquare())
+            {
+                Console.WriteLine("Это квадрат.");
+            }
+            else
+            {
+                Console.WriteLine("Это не квадрат.");
+            }
         }
     }
 }
