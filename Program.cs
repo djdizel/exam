c92ac6b521a9ebf0b24775feb17b235b5b32f1fb
djdizel <yarmisikov@gmail.com>
2025-06-25 18:11:14 +0300

    a

--- Program.cs
+++ Program.cs
@@ -10,6 +10,37 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Введите фамилию (или 'Q' для выхода): ");
+                    string fam = Console.ReadLine();
+                    if (fam.ToUpper() == "Q")
+                        break;
+
+                    Console.Write("Введите количество мест: ");
+                    int size = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите комфортность: ");
+                    string comfort = Console.ReadLine();
+
+                    Zakaz zakaz = new Zakaz(fam, size, comfort);
+                    Console.WriteLine("\nСоздан заказ:");
+                    zakaz.Show();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: количество мест должно быть числом!");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
         }
     }
 }
---
+++ Zakaz.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Zakaz
+    {
+        private string fam;
+        private int size;
+        private string comfort;
+
+        public string Fam
+        {
+            get { return fam; }
+            set { fam = value; }
+        }
+
+        public int Size
+        {
+            get { return size; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Количество мест должно быть положительным!");
+                size = value;
+            }
+        }
+
+        public string Comfort
+        {
+            get { return comfort; }
+            set { comfort = value; }
+        }
+
+        public Zakaz()
+        {
+            fam = "Не указано";
+            size = 1;
+            comfort = "Стандарт";
+        }
+
+        public Zakaz(string fam)
+        {
+            this.fam = fam;
+            size = 1;
+            comfort = "Стандарт";
+        }
+
+        public Zakaz(string fam, int size)
+        {
+            this.fam = fam;
+            Size = size;
+            comfort = "Стандарт";
+        }
+
+        public Zakaz(string fam, int size, string comfort)
+        {
+            this.fam = fam;
+            Size = size;
+            this.comfort = comfort;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Фамилия: {fam}");
+            Console.WriteLine($"Количество мест: {size}");
+            Console.WriteLine($"Комфортность: {comfort}");
+            Console.WriteLine();
+        }
+    }
+}
