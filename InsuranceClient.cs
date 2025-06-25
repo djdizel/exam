e990e7d3672ed7650d9c702beeee862afdfd3b84
djdizel <yarmisikov@gmail.com>
2025-06-25 05:50:47 +0300

    a

---
+++ InsuranceClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class InsuranceClient
+    {
+        private string name;
+        private string insuranceType;
+        private double insuranceAmount;
+
+        public InsuranceClient(string name, string insuranceType, double insuranceAmount)
+        {
+            this.name = name;
+            this.insuranceType = insuranceType;
+            this.insuranceAmount = insuranceAmount;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Клиент: " + name);
+            Console.WriteLine("Вид страховки: " + insuranceType);
+            Console.WriteLine("Размер страховки: " + String.Format("{0:F2}", insuranceAmount));
+            Console.WriteLine();
+        }
+
+        public string InsuranceType
+        {
+            get { return insuranceType; }
+        }
+
+        public double InsuranceAmount
+        {
+            get { return insuranceAmount; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,24 +10,53 @@ namespace exam
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите действительную часть первого числа: ");
-            double real1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите мнимую часть первого числа: ");
-            double imag1 = Convert.ToDouble(Console.ReadLine());
+            try
+            {
+                Console.Write("Введите количество клиентов: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                InsuranceClient[] clients = new InsuranceClient[n];
 
-            Console.Write("Введите действительную часть второго числа: ");
-            double real2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите мнимую часть второго числа: ");
-            double imag2 = Convert.ToDouble(Console.ReadLine());
+                int i;
+                for (i = 0; i < n; i++)
+                {
+                    Console.WriteLine("\nКлиент " + (i + 1) + ":");
+                    Console.Write("Имя: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Вид страховки: ");
+                    string type = Console.ReadLine();
+                    Console.Write("Размер страховки: ");
+                    double amount = Convert.ToDouble(Console.ReadLine());
 
-            Complex complex1 = new Complex(real1, imag1);
-            Complex complex2 = new Complex(real2, imag2);
+                    clients[i] = new InsuranceClient(name, type, amount);
+                }
 
-            Console.WriteLine("Первое число: " + complex1.ToString());
-            Console.WriteLine("Второе число: " + complex2.ToString());
-            Console.WriteLine("Сложение: " + complex1.Add(complex2).ToString());
-            Console.WriteLine("Вычитание: " + complex1.Subtract(complex2).ToString());
-            Console.WriteLine("Умножение: " + complex1.Multiply(complex2).ToString());
+                Console.WriteLine("\nКлиенты с автостраховкой на сумму > 2000 руб.:");
+                int autoCount = 0;
+                bool found = false;
+                for (i = 0; i < clients.Length; i++)
+                {
+                    if (clients[i].InsuranceType.ToLower() == "автомобиль" && clients[i].InsuranceAmount > 2000)
+                    {
+                        clients[i].ShowInfo();
+                        found = true;
+                        autoCount++;
+                    }
+                }
+                if (!found)
+                {
+                    Console.WriteLine("Клиенты не найдены.");
+                }
+
+                Console.WriteLine("Количество клиентов с автостраховкой: " + autoCount);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введено не число!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
         }
     }
 }
