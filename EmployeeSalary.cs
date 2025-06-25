2e18d43e7b863d250af405d58219251a3da78dc2
djdizel <yarmisikov@gmail.com>
2025-06-25 05:12:03 +0300

    a

---
+++ EmployeeSalary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class EmployeeSalary
+    {
+        private string name;
+        private double salary;
+        private double bonus;
+
+        public EmployeeSalary(string name, double salary, double bonus)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.bonus = bonus;
+        }
+
+        public double CalculateTotalSalary()
+        {
+            return salary + bonus;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Имя: " + name);
+            Console.WriteLine("Зарплата: " + String.Format("{0:F2}", salary));
+            Console.WriteLine("Премия: " + String.Format("{0:F2}", bonus));
+            Console.WriteLine("Итоговая зарплата: " + String.Format("{0:F2}", CalculateTotalSalary()));
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,36 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество сотрудников: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            EmployeeSalary[] employees = new EmployeeSalary[n];
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nВведите данные для сотрудника " + (i + 1) + ":");
+                Console.Write("Имя: ");
+                string name = Console.ReadLine();
+                Console.Write("Зарплата: ");
+                double salary = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Премия: ");
+                double bonus = Convert.ToDouble(Console.ReadLine());
+
+                employees[i] = new EmployeeSalary(name, salary, bonus);
+            }
+
+            Console.WriteLine("\nСотрудник с максимальной итоговой зарплатой:");
+            int maxIndex = 0;
+            double maxSalary = employees[0].CalculateTotalSalary();
+            for (i = 1; i < n; i++)
+            {
+                if (employees[i].CalculateTotalSalary() > maxSalary)
+                {
+                    maxSalary = employees[i].CalculateTotalSalary();
+                    maxIndex = i;
+                }
+            }
+            employees[maxIndex].ShowInfo();
         }
     }
 }
