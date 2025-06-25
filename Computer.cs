6f6ba35d2af34249df24d6fba450b5959ed224a3
djdizel <yarmisikov@gmail.com>
2025-06-25 18:00:49 +0300

    a

---
+++ Computer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Computer
+    {
+        private string model;
+        private int ram;
+        private int hdd;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Ram
+        {
+            get { return ram; }
+            set
+            {
+                if (value >= 2 && value <= 32)
+                {
+                    ram = value;
+                }
+                else
+                {
+                    throw new ArgumentException("ОЗУ должно быть от 2 до 32 ГБ!");
+                }
+            }
+        }
+
+        public int Hdd
+        {
+            get { return hdd; }
+            set
+            {
+                if (value >= 200 && value <= 2000)
+                {
+                    hdd = value;
+                }
+                else
+                {
+                    throw new ArgumentException("HDD должно быть от 200 до 2000 ГБ!");
+                }
+            }
+        }
+
+        public Computer(string model, int ram, int hdd)
+        {
+            this.model = model;
+            Ram = ram;
+            Hdd = hdd;
+        }
+
+        public double CalculateCost()
+        {
+            return 500 + ram * 50 + hdd * 0.5;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,28 +10,37 @@ namespace exam
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите номер счёта: ");
-            string accountNumber = Console.ReadLine();
-            Console.Write("Введите начальный баланс: ");
-            double balance = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Введите тип счёта (сберегательный/текущий): ");
-            string accountType = Console.ReadLine();
+            try
+            {
+                Computer[] computers = new Computer[]
+                {
+                    new Computer("Model1", 8, 500),
+                    new Computer("Model2", 16, 1000),
+                    new Computer("Model3", 32, 2000)
+                };
 
-            BankAccount account = new BankAccount(accountNumber, balance, accountType);
-
-            Console.WriteLine("\nИнформация о счёте:");
-            account.ShowInfo();
-
-            Console.Write("Введите сумму для пополнения: ");
-            double depositAmount = Convert.ToDouble(Console.ReadLine());
-            account.Deposit(depositAmount);
-
-            Console.Write("Введите сумму для снятия: ");
-            double withdrawAmount = Convert.ToDouble(Console.ReadLine());
-            account.Withdraw(withdrawAmount);
-
-            Console.WriteLine("\nОбновлённая информация о счёте:");
-            account.ShowInfo();
+                double totalCost = 0;
+                int i;
+                for (i = 0; i < computers.Length; i++)
+                {
+                    double cost = computers[i].CalculateCost();
+                    totalCost += cost;
+                    Console.WriteLine("Модель: " + computers[i].Model);
+                    Console.WriteLine("ОЗУ: " + computers[i].Ram + " ГБ");
+                    Console.WriteLine("HDD: " + computers[i].Hdd + " ГБ");
+                    Console.WriteLine("Стоимость: " + String.Format("{0:F2}", cost));
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Общая стоимость: " + String.Format("{0:F2}", totalCost));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
         }
     }
 }
