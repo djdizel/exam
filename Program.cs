6fa3fc39e3056b6d78995c45593d6cea16d3ce60
djdizel <yarmisikov@gmail.com>
2025-06-25 05:10:39 +0300

    +

--- Program.cs
+++ Program.cs
@@ -10,6 +10,39 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество студентов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Student[] students = new Student[n];
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nВведите данные для студента " + (i + 1) + ":");
+                Console.Write("Имя: ");
+                string name = Console.ReadLine();
+                Console.Write("Фамилия: ");
+                string surname = Console.ReadLine();
+                Console.Write("Средний балл: ");
+                double averageGrade = Convert.ToDouble(Console.ReadLine());
+
+                students[i] = new Student(name, surname, averageGrade);
+            }
+
+            Console.WriteLine("\nОтличники (средний балл >= 4.5):");
+            bool found = false;
+            for (i = 0; i < n; i++)
+            {
+                if (students[i].IsExcellent())
+                {
+                    Console.WriteLine("Студент " + (i + 1) + ":");
+                    students[i].ShowInfo();
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("Отличники не найдены.");
+            }
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Student
+    {
+        private string name;
+        private string surname;
+        private double averageGrade;
+
+        public Student(string name, string surname, double averageGrade)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.averageGrade = averageGrade;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Имя: " + name);
+            Console.WriteLine("Фамилия: " + surname);
+            Console.WriteLine("Средний балл: " + String.Format("{0:F2}", averageGrade));
+            Console.WriteLine();
+        }
+
+        public bool IsExcellent()
+        {
+            if (averageGrade >= 4.5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
