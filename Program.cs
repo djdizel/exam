5b0f85899813a92576d71221f4a4d50d9592a340
djdizel <yarmisikov@gmail.com>
2025-06-25 05:20:08 +0300

    a

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите x для первого вектора: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y для первого вектора: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите x для второго вектора: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y для второго вектора: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Vector vector1 = new Vector(x1, y1);
+            Vector vector2 = new Vector(x2, y2);
+
+            Console.WriteLine("\nПервый вектор:");
+            vector1.ShowInfo();
+            Console.WriteLine("\nВторой вектор:");
+            vector2.ShowInfo();
+            Console.WriteLine("\nСкалярное произведение: " + String.Format("{0:F2}", vector1.ScalarProduct(vector2)));
         }
     }
 }
---
+++ Vector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Vector
+    {
+        private double x;
+        private double y;
+
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double CalculateLength()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public double ScalarProduct(Vector other)
+        {
+            return x * other.x + y * other.y;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Вектор: (" + x + ", " + y + ")");
+            Console.WriteLine("Длина вектора: " + String.Format("{0:F2}", CalculateLength()));
+        }
+    }
+}
