c1e3973a95166bff49dc158e7a7500e282326c01
djdizel <yarmisikov@gmail.com>
2025-06-25 05:16:45 +0300

    a

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите первую сторону треугольника: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите вторую сторону треугольника: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите третью сторону треугольника: ");
+            double side3 = Convert.ToDouble(Console.ReadLine());
+
+            Triangle triangle = new Triangle(side1, side2, side3);
+
+            if (triangle.Exists())
+            {
+                Console.WriteLine("Треугольник существует.");
+                Console.WriteLine("Площадь треугольника: " + String.Format("{0:F2}", triangle.CalculateArea()));
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не существует.");
+            }
         }
     }
 }
