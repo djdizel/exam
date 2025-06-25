3863c268a62ff77d3b401e46e08fab55472d869d
djdizel <yarmisikov@gmail.com>
2025-06-25 05:46:21 +0300

    a

--- Program.cs
+++ Program.cs
@@ -8,63 +8,39 @@ namespace exam
 {
     internal class Program
     {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Введите количество поездов: ");
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            Train[] trains = new Train[n];
-            int i;
-            for (i = 0; i < n; i++)
+            try
             {
-                Console.WriteLine("\nВведите данные для поезда " + (i + 1) + ":");
-                Console.Write("Пункт назначения: ");
-                string destination = Console.ReadLine();
-                Console.Write("Номер поезда: ");
-                int trainNumber = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Время отправления (чч:мм): ");
-                DateTime departureTime = DateTime.Parse(Console.ReadLine());
+                MyClass myClass = new MyClass();
+                myClass.change = "не изменено";
+                MyStruct myStruct = new MyStruct();
+                myStruct.change = "не изменено";
 
-                trains[i] = new Train(destination, trainNumber, departureTime);
+                Console.WriteLine("До вызова методов:");
+                Console.WriteLine("Класс: " + myClass.change);
+                Console.WriteLine("Структура: " + myStruct.change);
+
+                ClassTaker(myClass);
+                StruktTaker(myStruct);
+
+                Console.WriteLine("\nПосле вызова методов:");
+                Console.WriteLine("Класс: " + myClass.change); // Изменится
+                Console.WriteLine("Структура: " + myStruct.change); // Не изменится
             }
-
-            // Сортировка по пункту назначения
-            for (i = 0; i < trains.Length - 1; i++)
+            catch (Exception ex)
             {
-                for (int j = 0; j < trains.Length - i - 1; j++)
-                {
-                    if (String.Compare(trains[j].Destination, trains[j + 1].Destination) > 0)
-                    {
-                        Train temp = trains[j];
-                        trains[j] = trains[j + 1];
-                        trains[j + 1] = temp;
-                    }
-                }
-            }
-
-            Console.WriteLine("\nОтсортированный список поездов:");
-            for (i = 0; i < trains.Length; i++)
-            {
-                Console.WriteLine("Поезд " + (i + 1) + ":");
-                trains[i].ShowInfo();
-            }
-
-            Console.Write("\nВведите номер поезда для поиска: ");
-            int searchNumber = Convert.ToInt32(Console.ReadLine());
-            bool found = false;
-            for (i = 0; i < trains.Length; i++)
-            {
-                if (trains[i].TrainNumber == searchNumber)
-                {
-                    Console.WriteLine("\nНайден поезд:");
-                    trains[i].ShowInfo();
-                    found = true;
-                    break;
-                }
-            }
-            if (!found)
-            {
-                Console.WriteLine("Поезд с номером " + searchNumber + " не найден.");
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
             }
         }
     }
