35c8f3d9accd63c38420cb75c743440eaebbb1f1
djdizel <yarmisikov@gmail.com>
2025-06-25 05:18:49 +0300

    a

---
+++ Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Product
+    {
+        private string name;
+        private double price;
+        private int quantity;
+
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public double CalculateTotalCost()
+        {
+            return price * quantity;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Название: " + name);
+            Console.WriteLine("Цена: " + String.Format("{0:F2}", price));
+            Console.WriteLine("Количество: " + quantity);
+            Console.WriteLine("Общая стоимость: " + String.Format("{0:F2}", CalculateTotalCost()));
+            Console.WriteLine();
+        }
+
+        public double Price
+        {
+            get { return price; }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,36 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество продуктов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Product[] products = new Product[n];
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("\nВведите данные для продукта " + (i + 1) + ":");
+                Console.Write("Название: ");
+                string name = Console.ReadLine();
+                Console.Write("Цена: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Количество: ");
+                int quantity = Convert.ToInt32(Console.ReadLine());
+
+                products[i] = new Product(name, price, quantity);
+            }
+
+            Console.WriteLine("\nПродукт с минимальной ценой:");
+            int minIndex = 0;
+            double minPrice = products[0].Price;
+            for (i = 1; i < n; i++)
+            {
+                if (products[i].Price < minPrice)
+                {
+                    minPrice = products[i].Price;
+                    minIndex = i;
+                }
+            }
+            products[minIndex].ShowInfo();
         }
     }
 }
