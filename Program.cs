bd1d55ae3e1e012f13db647a741de8e5e9ac4433
djdizel <yarmisikov@gmail.com>
2025-06-25 18:07:31 +0300

    a

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Segment<int> intSegment = new Segment<int>(0, 0, 3, 4);
+                Console.WriteLine("Отрезок с int:");
+                intSegment.ShowInfo();
+
+                Segment<long> longSegment = new Segment<long>(1, 2, 5, 6);
+                Console.WriteLine("Отрезок с long:");
+                longSegment.ShowInfo();
+
+                Segment<double> doubleSegment = new Segment<double>(1.5, 2.5, 4.5, 6.5);
+                Console.WriteLine("Отрезок с double:");
+                doubleSegment.ShowInfo();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
         }
     }
 }
---
+++ Segment.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class Segment<T>
+    {
+        private T x1, y1, x2, y2;
+
+        public T X1
+        {
+            get { return x1; }
+            set { x1 = value; }
+        }
+
+        public T Y1
+        {
+            get { return y1; }
+            set { y1 = value; }
+        }
+
+        public T X2
+        {
+            get { return x2; }
+            set { x2 = value; }
+        }
+
+        public T Y2
+        {
+            get { return y2; }
+            set { y2 = value; }
+        }
+
+        public Segment(T x1, T y1, T x2, T y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public double CalculateLength()
+        {
+            double dx = Convert.ToDouble(x2) - Convert.ToDouble(x1);
+            double dy = Convert.ToDouble(y2) - Convert.ToDouble(y1);
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Отрезок: [({x1}, {y1}), ({x2}, {y2})]");
+            Console.WriteLine($"Длина: {CalculateLength():F2}");
+            Console.WriteLine();
+        }
+    }
+}
