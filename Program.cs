45ae872cf0551698dca88b4fe54e23372a7f668d
djdizel <yarmisikov@gmail.com>
2025-06-25 05:53:32 +0300

    ф

--- Program.cs
+++ Program.cs
@@ -8,35 +8,69 @@ namespace exam
 {
     internal class Program
     {
+        static void PrintList(List<string> list)
+        {
+            Console.WriteLine("Список цветов:");
+            int i;
+            for (i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Введите первую строку: ");
-            string str1 = Console.ReadLine();
-            MyString myString1 = new MyString(str1);
-
-            Console.Write("Введите вторую строку для объединения: ");
-            string str2 = Console.ReadLine();
-            MyString myString2 = new MyString(str2);
-
-            Console.WriteLine("Объединённая строка: " + myString1.Concatenate(myString2));
-
-            Console.Write("Введите подстроку для поиска: ");
-            string substring = Console.ReadLine();
-            int pos = myString1.FindSubstring(substring);
-            if (pos != -1)
+            try
             {
-                Console.WriteLine("Подстрока найдена на позиции: " + pos);
-            }
-            else
-            {
-                Console.WriteLine("Подстрока не найдена");
-            }
+                List<string> colors = new List<string>();
 
-            Console.Write("Введите подстроку для замены: ");
-            string oldSubstring = Console.ReadLine();
-            Console.Write("Введите новую подстроку: ");
-            string newSubstring = Console.ReadLine();
-            Console.WriteLine("Результат замены: " + myString1.ReplaceSubstring(oldSubstring, newSubstring));
+                // 1. Добавление строчными буквами
+                colors.Add("красный");
+                colors.Add("синий");
+                colors.Add("зелёный");
+                Console.WriteLine("После добавления:");
+                PrintList(colors);
+
+                // 2. Преобразование в прописные
+                int i;
+                for (i = 0; i < colors.Count; i++)
+                {
+                    colors[i] = colors[i].ToUpper();
+                }
+                Console.WriteLine("После преобразования в прописные:");
+                PrintList(colors);
+
+                // 3. Количество элементов
+                Console.WriteLine("Текущее количество элементов: " + colors.Count);
+
+                // 4. Добавление в конец
+                colors.Add("жёлтый");
+                Console.WriteLine("После добавления в конец:");
+                PrintList(colors);
+
+                // 5. Добавление в начало
+                colors.Insert(0, "белый");
+                Console.WriteLine("После добавления в начало:");
+                PrintList(colors);
+
+                // 6. Удаление третьего элемента
+                if (colors.Count >= 3)
+                {
+                    colors.RemoveAt(2);
+                }
+                Console.WriteLine("После удаления третьего элемента:");
+                PrintList(colors);
+
+                // 7. Реверс
+                colors.Reverse();
+                Console.WriteLine("После реверса:");
+                PrintList(colors);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
         }
     }
 }
