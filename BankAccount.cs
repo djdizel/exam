bbb53074c0f4897521e7983bfc859749fc92d629
djdizel <yarmisikov@gmail.com>
2025-06-25 05:19:32 +0300

    a

---
+++ BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam
+{
+    class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+        private string accountType;
+
+        public BankAccount(string accountNumber, double balance, string accountType)
+        {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+            if (accountType != "сберегательный" && accountType != "текущий")
+            {
+                Console.WriteLine("Ошибка: тип счёта должен быть 'сберегательный' или 'текущий'!");
+                this.accountType = "текущий";
+            }
+            else
+            {
+                this.accountType = accountType;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine("Счёт пополнен на " + String.Format("{0:F2}", amount));
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: сумма пополнения должна быть положительной!");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine("Снято " + String.Format("{0:F2}", amount));
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: недостаточно средств или некорректная сумма!");
+            }
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Номер счёта: " + accountNumber);
+            Console.WriteLine("Баланс: " + String.Format("{0:F2}", balance));
+            Console.WriteLine("Тип счёта: " + accountType);
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите номер счёта: ");
+            string accountNumber = Console.ReadLine();
+            Console.Write("Введите начальный баланс: ");
+            double balance = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите тип счёта (сберегательный/текущий): ");
+            string accountType = Console.ReadLine();
+
+            BankAccount account = new BankAccount(accountNumber, balance, accountType);
+
+            Console.WriteLine("\nИнформация о счёте:");
+            account.ShowInfo();
+
+            Console.Write("Введите сумму для пополнения: ");
+            double depositAmount = Convert.ToDouble(Console.ReadLine());
+            account.Deposit(depositAmount);
+
+            Console.Write("Введите сумму для снятия: ");
+            double withdrawAmount = Convert.ToDouble(Console.ReadLine());
+            account.Withdraw(withdrawAmount);
+
+            Console.WriteLine("\nОбновлённая информация о счёте:");
+            account.ShowInfo();
         }
     }
 }
