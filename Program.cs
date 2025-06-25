878be72f3daa5a879f81adb4d7d41db21a8a9e53
djdizel <yarmisikov@gmail.com>
2025-06-25 05:17:29 +0300

    a

--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace exam
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите x для первой точки: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y для первой точки: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите z для первой точки: ");
+            double z1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите x для второй точки: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y для второй точки: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите z для второй точки: ");
+            double z2 = Convert.ToDouble(Console.ReadLine());
+
+            Point3D point1 = new Point3D(x1, y1, z1);
+            Point3D point2 = new Point3D(x2, y2, z2);
+
+            Console.WriteLine("\nПервая точка:");
+            point1.ShowInfo();
+            Console.WriteLine("Вторая точка:");
+            point2.ShowInfo();
+            Console.WriteLine("Расстояние между точками: " + String.Format("{0:F2}", point1.DistanceTo(point2)));
         }
     }
 }
